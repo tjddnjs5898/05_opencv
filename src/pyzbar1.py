c5713f4fe515a55ba3bbec5276408d0915bf1983
tjddnjs5898 <tjddnjs5898@naver.com>
2025-08-01 10:40:32 +0900

    디코딩

--- src/pyzbar1.py
+++ src/pyzbar1.py
@@ -1,10 +1,18 @@
 import cv2
 import matplotlib.pyplot as plt
-#import pyzbar.pyzbar as pyzbar
+import pyzbar.pyzbar as pyzbar
 
 img = cv2.imread('../img/frame.png')
-plt.imshow(img)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#plt.imshow(img)
+plt.imshow(gray, cmap='gray')
 plt.show()
+
+# 디코딩
+
+decoded = pyzbar.decode(gray)
+print(decoded)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
